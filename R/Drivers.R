1aa46e533852173b3eff39e7fd55a6108f7dfafc
Leonardini <leonid@sfu.ca>
2018-07-20 00:56:03 -0700

    added stat comparison

--- R/Drivers.R
+++ R/Drivers.R
@@ -6,7 +6,7 @@ networkStatNames  = c("diameter", "WienerIndex", "betweenness", "closeness", "ei
 #' Basic summary statistics of a rooted phylo tree.
 #'
 #' \code{computeBasicStats} computes basic summary statistics of a rooted binary phylo tree.
-#' @param tree A phylo tree; needs to be binary and rooted.
+#' @param tree A tree of class \code{phylo}. The tree should be binary and rooted; if it is not, it will be coerced into a rooted tree using \code{root} and/or binarized using \code{multi2di}.
 #' @return A named vector containing the 16 basic summary statistics.
 #' @family drivers for computing summary statistics
 #' @export
@@ -33,7 +33,7 @@ computeBasicStats = function(tree) {
 #' Network science-based statistics of a rooted phylo tree.
 #'
 #' \code{computeNetworksStats} computes network science-based statistics of a rooted binary phylo tree.
-#' @param tree A phylo tree; needs to be binary and rooted.
+#' @inheritParams computeBasicStats
 #' @param weight A logical scalar; if TRUE, the branch lengths are taken into account.
 #' @param meanpath A logical scalar; if TRUE, the Wiener index is normalized to the mean path.
 #' @param maxOnly A logical scalar; if TRUE, only the maximum value of each vector is returned.
@@ -61,18 +61,19 @@ computeNetworkStats = function(tree, weight = FALSE, meanpath = FALSE, maxOnly =
 #' Spectra of a rooted phylo tree.
 #'
 #' \code{computeSpectralStats} computes various spectra of a rooted binary phylo tree.
-#' @param tree A phylo tree; needs to be binary and rooted.
+#' @inheritParams computeBasicStats
+#' @inheritParams computeNetworkStats
 #' @param weight A logical vector; a TRUE (FALSE) entry means the branch lengths are (not) taken into account.
 #' @param adj A logical vector; a TRUE (FALSE) entry means the adjacency (Laplacian) spectrum is computed.
 #' @param norm A logical vector; a TRUE (FALSE) entry means the Laplacian is (not) normalized.
 #' @param dist A logical vector; a TRUE (FALSE) entry means the distance (regular) matrix is used.
 #' @param full A logical vector;  a TRUE (FALSE) entry means the full (leaf-leaf) distance matrix is used.
-#' @param maxOnly A logical scalar; if TRUE, only the maximum value of each vector is returned.
 #' @return A named list containing the specified spectra (or only their maximum values).
 #' @family drivers for computing summary statistics
 #' @export
 computeSpectralStats = function(tree, weight = c(FALSE, TRUE), adj = c(FALSE, TRUE),
   norm = FALSE, dist = FALSE, full = FALSE, maxOnly = TRUE) {
+  tree = checkPhylogeneticTree(tree)
   normFactor = sum(ifelse(adj, 1, length(norm))) # norm only applies to adj = FALSE (lap)
   distFactor = sum(ifelse(dist, length(full), 1)) # full only applied to dist = TRUE
   numSpectra = length(weight) * length(adj) * normFactor * distFactor
@@ -102,4 +103,59 @@ computeSpectralStats = function(tree, weight = c(FALSE, TRUE), adj = c(FALSE, TR
     allSpectra = sapply(allSpectra, max)
   }
   allSpectra
+}
+
+### TODO: ADD DESCRIPTION!
+rankDiscriminatoryStats = function(treeList1, treeList2, basic = TRUE, network = TRUE, spectral = TRUE) {
+  Lens = c(length(treeList1), length(treeList2))
+  if (Lens[1] != Lens[2]) {
+    warning("Different numbers of trees are being compared")
+  }
+  L1 = sapply(treeList1, Ntip)
+  L2 = sapply(treeList2, Ntip)
+  if (mean(L1, na.rm = TRUE) != mean(L2, na.rm = TRUE)) {
+    warning("The average numbers of tips are not equal")
+  }
+  numStats = length(basicStatNames) * basic + length(networkStatNames) * network + 4 * spectral
+  Stats = matrix(NA, Lens[1] + Lens[2], numStats)
+  for (index in 1:2) {
+    startRow = ifelse(index == 1, 0, Lens[1])
+    for (ind in 1:Lens[index]) {
+      curCol = 0
+      curTree = treeLists[[index]][[ind]]
+      if (basic) {
+        Stats[startRow + ind, curCol + (1:length(basicStatNames))] = computeBasicStats(curTree)
+        curCol = curCol + length(basicStatNames)
+      }
+      if (network) {
+        Stats[startRow + ind, curCol + (1:length(networkStatNames))] = computeNetworkStats(curTree)
+        curCol = curCol + length(networkStatNames)
+      }
+      if (spectral) {
+        
+      }
+    }
+  }
+  Stats1 = Stats[1:Lens[1], , drop = FALSE]
+  Stats2 = Stats[-(1:Lens[1]), , drop = FALSE]
+  output = compareStats(Stats1, Stats2)
+  output
+}
+
+### TODO: ADD DESCRIPTION!
+compareStats = function(Stats1, Stats2) {
+  stopifnot(ncol(Stats1) == ncol(Stats2))
+  stopifnot(all(sort(colnames(Stats1)) == sort(colnames(Stats2))))
+  Mat = matrix(NA, ncol(Stats1), 3, dimnames = list(colnames(Stats1), c("mean1", "mean2", "p-value")))
+  for (ind in 1:ncol(Stats1)) {
+    curName = colnames(Stats1)[ind]
+    cur1 = Stats1[,curName]
+    cur2 = Stats2[,curName]
+    curTest = t.test(unlist(cur1), unlist(cur2))
+    curResult = c(curTest$estimate, curTest$p.value)
+    names(curResult) = NULL
+    Mat[ind,] = curResult
+  }
+  Mat = Mat[order(Mat[, "p-value"]), ]
+  Mat
 }
